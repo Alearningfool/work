e233df97712e20684d0480118e6eb9306d2ee445
smzdm <715576594@qq.com>
2021-02-09 15:12:41 +0800

    这是一次python文件提交

--- first.py
+++ first.py
@@ -2,10 +2,10 @@
 Description: 
 Author: 雷佳林
 Date: 2021-02-09 14:38:28
-LastEditTime: 2021-02-09 14:39:18
+LastEditTime: 2021-02-09 15:11:29
 LastEditors: 雷佳林
 '''
 
 
-name = input('请输入你的名字：')
+name = input('请输入你的名字123：')
 print('hello,',name)
