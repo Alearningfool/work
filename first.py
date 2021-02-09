99296d032d27ccb8e4564c410328fc3adfc5af28
smzdm <715576594@qq.com>
2021-02-09 15:02:21 +0800

    222

---
+++ first.py
@@ -0,0 +1,11 @@
+'''
+Description: 
+Author: 雷佳林
+Date: 2021-02-09 14:38:28
+LastEditTime: 2021-02-09 14:39:18
+LastEditors: 雷佳林
+'''
+
+
+name = input('请输入你的名字：')
+print('hello,',name)
---
+++ hello.py
@@ -0,0 +1,9 @@
+'''
+Description: 
+Author: 雷佳林
+Date: 2021-02-09 14:24:42
+LastEditTime: 2021-02-09 14:29:03
+LastEditors: 雷佳林
+'''
+#!/usr/bin/env python3
+print('hello world')
